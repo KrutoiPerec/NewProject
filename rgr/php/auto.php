5a33ac81ba54c95c3ee210567f03c332ffb0b99f
Aisultan <58466812+AisSaulabev@users.noreply.github.com>
2020-04-20 11:44:29 +0600

    111

--- rgr/php/auto.php
+++ rgr/php/auto.php
@@ -15,27 +15,21 @@ $link=mysqli_connect("localhost", "root", "", "Movie");
 if(isset($_POST['submit']))
 {
     // Вытаскиваем из БД запись, у которой логин равняеться введенному
-    $query = mysqli_query($link,"SELECT user_id, user_password FROM users WHERE user_login='".mysqli_real_escape_string($link,$_POST['login'])."' LIMIT 1");
+    $query = mysqli_query($link,"SELECT Password FROM users WHERE Name='".mysqli_real_escape_string($link,$_POST['user_name'])."' LIMIT 1");
     $data = mysqli_fetch_assoc($query);
 
     // Сравниваем пароли
-    if($data['user_password'] === md5(md5($_POST['password'])))
+    if($data['Password'] === md5(md5($_POST['user_password'])))
     {
         // Генерируем случайное число и шифруем его
         $hash = md5(generateCode(10));
 
-        if(!empty($_POST['not_attach_ip']))
-        {
-            // Если пользователя выбрал привязку к IP
-            // Переводим IP в строку
-            $insip = ", user_ip=INET_ATON('".$_SERVER['REMOTE_ADDR']."')";
-        }
-
+       
         // Записываем в БД новый хеш авторизации и IP
-        mysqli_query($link, "UPDATE users SET user_hash='".$hash."' ".$insip." WHERE user_id='".$data['user_id']."'");
+        mysqli_query($link, "UPDATE users SET user_hash='".$hash."' ".$insip." WHERE Name='".$data['user_name']."'");
 
         // Ставим куки
-        setcookie("id", $data['user_id'], time()+60*60*24*30, "/");
+        setcookie($data['user_name'], time()+60*60*24*30, "/");
         setcookie("hash", $hash, time()+60*60*24*30, "/", null, null, true); // httponly !!!
 
         // Переадресовываем браузер на страницу проверки нашего скрипта
