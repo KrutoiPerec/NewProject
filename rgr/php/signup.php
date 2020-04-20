4aa474204118a2e040b0236cacaf25b3c306a44e
Aisultan <58466812+AisSaulabev@users.noreply.github.com>
2020-04-20 15:28:34 +0600

    33

--- rgr/php/signup.php
+++ rgr/php/signup.php
@@ -1,21 +1,36 @@
 <?php
-    
+
     require_once 'connection.php';
     $user_name = $_POST['user_name'];
     $user_mail = $_POST['user_mail'];
     $user_passwd = $_POST['user_password'];
-    $hash = password_hash($password,PASSWORD_DEFAULT);
-  
-    $result = mysqli_query($connection, "INSERT INTO users (`Name`, `Mail`, `Password`) 
-    VALUES ('$user_name', '$user_mail' , '$hash')");
+
+
+    $ids_array = array();
+    $names = mysqli_query($connection ,"SELECT Name FROM users");
+    while($row = mysqli_fetch_array($names))
+    {
+        $ids_array[] = $row['Name'];
+    }
+
+    foreach ($ids_array as $name) {
+        if($user_name == $name)
+        {
+            echo"<script type='text/javascript'>alert('Пользователь уже существует'); window.location.href='../reg/registration.html';</script>";
+            die();
+        }
+    }
+
+    $result = mysqli_query($connection, "INSERT INTO users (`Name`, `Mail`, `Password`)
+    VALUES ('$user_name', '$user_mail' , '$user_passwd')");
             if($result)
             {
-                echo"<script type='text/javascript'>alert('Успешно'); window.location.href='../index.html';</script>";
+                echo"<script type='text/javascript'>alert('Успешно'); window.location.href='../index.php';</script>";
             }
             else
             {
                 echo $user_name,$user_mail,$user_passwd;
                 echo "ошибка";
-            } 
-         
+            }
+
             ?>
