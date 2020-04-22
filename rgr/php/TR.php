cb343edf58dc46312167656227bc2c8e3c10c583
Aisultan <58466812+AisSaulabev@users.noreply.github.com>
2020-04-22 18:04:39 +0600

    php

---
+++ rgr/php/TR.php
@@ -0,0 +1,19 @@
+<?php
+        require_once 'connection.php';
+        session_start();
+        $Text = $_POST['TextArea'];
+        $Name = $_SESSION['session_username'];
+        $Film = $_SESSION['film'];
+        $result = mysqli_query($connection, "INSERT INTO com (`Name`, `Text`, `Film`)
+        VALUES ('$Name', '$Text', '$Film')");
+            if($result)
+            {
+                echo"<script type='text/javascript'>alert('Успешно'); window.location.href='../treilers/$Film.php';</script>";
+            }
+            else
+            {
+                echo $Name." ".$Text." ".$Film." ";
+                echo "ошибка";
+            }
+
+?>
